30f9baa9f135e5fd3e73cdd4f4f3f7e53fa43936
Rahul Konda <rahulmadhukonda@outlook.com>
2023-12-26 04:06:41 +0000

    sort-functionality added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import Cookies from 'js-cookie'
 import {Switch, Route} from 'react-router-dom'
-import NavBar from './components/NavBar'
+import RestrauntsList from './components/RestrauntsList'
 import NotFound from './components/NotFound'
 import './App.css'
-
-/**     const sortByOptions = [
-  {
-    id: 0,
-    displayText: 'Highest',
-    value: 'Highest',
-  },
-  {
-    id: 2,
-    displayText: 'Lowest',
-    value: 'Lowest',
-  },
-]
-    * */
 
 const App = () => {
   Cookies.set('jwt_token', '15316546897486451564')
   return (
     <Switch>
-      <Route exact path="/" component={NavBar} />
+      <Route exact path="/" component={RestrauntsList} />
       <Route exact path="/not-found" component={NotFound} />
     </Switch>
   )
---
+++ src/components/RestrauntsList/index.js
@@ -0,0 +1,62 @@
+import {Component} from 'react'
+import {MdSort} from 'react-icons/md'
+import {TiArrowSortedDown} from 'react-icons/ti'
+import './index.css'
+
+const sortByOptions = [
+  {
+    id: 0,
+    displayText: 'Highest',
+    value: 'Highest',
+  },
+  {
+    id: 2,
+    displayText: 'Lowest',
+    value: 'Lowest',
+  },
+]
+
+class RestrauntsList extends Component {
+  state = {sortBy: sortByOptions[0].value}
+
+  onChangeSortBy = e => {
+    console.log('changed')
+    console.log(e.target.value)
+    this.setState({sortBy: e.target.value})
+  }
+
+  render() {
+    const {sortBy} = this.state
+    return (
+      <div className="restraunts-list-bg">
+        <div className="top-section">
+          <div className="descriptions">
+            <h1 className="pop-heading">Popular Restraunts</h1>
+            <p className="pop-desc">
+              Select Your favourite restaurant special dish and make your day
+              happy...
+            </p>
+          </div>
+          <div className="sort-box">
+            <MdSort size={25} color="#475569" />
+            <select
+              className="pop-select"
+              value={sortBy}
+              onChange={this.onChangeSortBy}
+            >
+              {sortByOptions.map(each => (
+                <option key={each.id} value={each.value}>
+                  {each.displayText}
+                </option>
+              ))}
+            </select>
+            <TiArrowSortedDown size={25} color="#475569" />
+          </div>
+        </div>
+        <hr className="hr" />
+      </div>
+    )
+  }
+}
+
+export default RestrauntsList
